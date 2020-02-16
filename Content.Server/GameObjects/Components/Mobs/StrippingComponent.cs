c537620080cbaa4e84c039dfcb38125cb8b26065
Unknown <Floydje123@hotmail.com>
2020-02-16 02:34:03 +0100

    die

---
+++ Content.Server/GameObjects/Components/Mobs/StrippingComponent.cs
@@ -0,0 +1,58 @@
+﻿using Robust.Shared.GameObjects;
+using Content.Server.GameObjects.EntitySystems;
+using Content.Shared.GameObjects;
+using Robust.Shared.IoC;
+
+namespace Content.Server.GameObjects.Components
+{
+    /// <summary>
+    ///     Allows an object with a InventoryComponent to be stripped from its items.
+    /// </summary>
+    [RegisterComponent]
+    [ComponentReference(typeof(IActivate))]
+    public class StrippingComponent : SharedStrippingComponent, IActivate
+    {
+        /// <inheritdoc />
+
+        [Dependency] private readonly IGameHud _gameHud;
+
+        void IActivate.Activate(ActivateEventArgs args)
+        {
+            Owner.TryGetComponent(out ClientStrippingComponent clientInventory);
+
+            var menu = clientInventory.InterfaceController.Window;
+
+            if (menu.IsOpen)
+            {
+                if (menu.IsAtFront())
+                {
+                    _setOpenValue(menu, false);
+                }
+                else
+                {
+                    menu.MoveToFront();
+                }
+            }
+            else
+            {
+                _setOpenValue(menu, true);
+            }
+        }
+        private void _setOpenValue(SS14Window menu, bool value)
+        {
+            if (value)
+            {
+                _gameHud.InventoryButtonDown = true;
+                menu.OpenCentered();
+            }
+            else
+            {
+                _gameHud.InventoryButtonDown = false;
+                menu.Close();
+            }
+        }
+
+
+    }
+
+}
---
+++ Content.Shared/GameObjects/Components/Inventory/SharedStrippingComponent.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using Robust.Shared.GameObjects;
+using Robust.Shared.IoC;
+using Robust.Shared.Interfaces.Reflection;
+using Robust.Shared.Serialization;
+using Robust.Shared.Utility;
+using Robust.Shared.ViewVariables;
+using static Content.Shared.GameObjects.Components.Inventory.EquipmentSlotDefines;
+
+namespace Content.Shared.GameObjects
+{
+    public abstract class SharedStrippingComponent : Component
+    {
+
+        [Dependency] protected readonly IReflectionManager ReflectionManager;
+        [Dependency] protected readonly IDynamicTypeFactory DynamicTypeFactory;
+        public sealed override string Name => "Stripping";
+        public sealed override uint? NetID => ContentNetIDs.STRIPPING;
+
+        [ViewVariables]
+        protected Inventory InventoryInstance { get; private set; }
+
+        [ViewVariables]
+        private string _templateName = "HumanInventory"; //stored for serialization purposes
+
+        public override void ExposeData(ObjectSerializer serializer)
+        {
+            base.ExposeData(serializer);
+
+            serializer.DataField(ref _templateName, "Template", "HumanInventory");
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            CreateInventory();
+        }
+
+        private void CreateInventory()
+        {
+            var type = ReflectionManager.LooseGetType(_templateName);
+            DebugTools.Assert(type != null);
+            InventoryInstance = DynamicTypeFactory.CreateInstance<Inventory>(type);
+        }
+
+        [Serializable, NetSerializable]
+        protected class InventoryComponentState : ComponentState
+        {
+            public List<KeyValuePair<Slots, EntityUid>> Entities { get; }
+
+            public InventoryComponentState(List<KeyValuePair<Slots, EntityUid>> entities) : base(ContentNetIDs.STORAGE)
+            {
+                Entities = entities;
+            }
+        }
+
+        [Serializable, NetSerializable]
+        public class ClientInventoryMessage : ComponentMessage
+        {
+            public Slots Inventoryslot;
+            public ClientInventoryUpdate Updatetype;
+
+            public ClientInventoryMessage(Slots inventoryslot, ClientInventoryUpdate updatetype)
+            {
+                Directed = true;
+                Inventoryslot = inventoryslot;
+                Updatetype = updatetype;
+            }
+
+            public enum ClientInventoryUpdate
+            {
+                Equip = 0,
+                Use = 1
+            }
+        }
+
+        /// <summary>
+        /// Component message for opening the Storage UI of item in Slot
+        /// </summary>
+        [Serializable, NetSerializable]
+        public class OpenSlotStorageUIMessage : ComponentMessage
+        {
+            public Slots Slot;
+
+            public OpenSlotStorageUIMessage(Slots slot)
+            {
+                Directed = true;
+                Slot = slot;
+            }
+        }
+    }
+}
